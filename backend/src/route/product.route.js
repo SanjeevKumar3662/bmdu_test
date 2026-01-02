50bb17431b639c63a4b1c2d32ac9baeee1dfb521
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-02 13:35:34 +0530

    feat: add updateProduct and deleteProduct routes to product API

--- backend/src/route/product.route.js
+++ backend/src/route/product.route.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
   getProduct,
+  updateProduct,
 } from "../controller/product.controller.js";
 
 const router = Router();
@@ -11,5 +13,7 @@ const router = Router();
 router.post("/products", asyncHandler(createProduct));
 router.get("/products", asyncHandler(getAllProducts));
 router.get("/products/:id", asyncHandler(getProduct));
+router.patch("/products/:id", asyncHandler(updateProduct));
+router.delete("/products/:id", asyncHandler(deleteProduct));
 
 export default router;
